45b300619cd3062d0a01ba95c8061af0318ae022
Rishi-1801 <rishikeshreddy018@gmail.com>
2026-07-08 14:29:18 +0530

    Text Splitters

---
+++ LangChain/Text_Splitters/length_based.py
@@ -0,0 +1,18 @@
+from langchain_classic.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader=PyPDFLoader('Document_Loaders\dl-curriculum.pdf')
+
+docs=loader.load()
+
+splitter=CharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=0,
+    separator=''
+)
+
+result=splitter.split_documents(docs)
+
+# print(result)  ---> returns the list of doc objects
+
+print(result[0].page_content)
